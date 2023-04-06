f15395ffb4bb59380fc4d1ebdfbe7faee2acf0d0
KishorExaltech <kishor.gunjal@exaltaretech.com>
2023-04-06 18:49:49 +0530

    first commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const YTAPI = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyCOuAaaudbMiMXYgHDt50kX8FCSE_bioJw"
+export const YTAPI = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=key"
 
 export const YOUTUBE_SEARCH = 'https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q='
